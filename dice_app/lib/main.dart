489e732429a71b9ce1aa2e2d1c6273f53a3e5480
tomroolfs <tomwillem04@gmail.com>
2024-03-04 12:36:31 +0100

    dice app afgemaakt

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,28 +1,65 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
-   const MaterialApp(
+    const MaterialApp(
       title: 'Flutter Demo',
       home: HomePage(),
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int leftDiceNumber = 1;
+
+  int rightDiceNumber = 2;
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 36, 36, 36),
+        backgroundColor: const Color.fromARGB(255, 0, 30, 57),
         foregroundColor: Colors.white,
         title: const Text(
-          'Home',
-          style: TextStyle(fontStyle: FontStyle.italic),
+          'Dice App',
         ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 156, 255, 177),
+      body: Center(
+        child: Row(children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ]),
       ),
     );
   }
